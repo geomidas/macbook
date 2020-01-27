f48405ea34ebe9a09c16b6ce4c784125033742fa
Georgios Keramidas <georgiosmidas@gmail.com>
2020-01-27 16:27:31 +0000

    fixed script: rm evicted pods

--- scripts/k8s/rm_evicted_pods.sh
+++ scripts/k8s/rm_evicted_pods.sh
@@ -4,5 +4,9 @@ evicted_pods="$(kubectl get pods | grep Evicted | awk '{print $1}')"
 
 if ! [ -z "$evicted_pods" ]
 then
-    kubectl delete pods "$evicted_pods"
+	for pod in ${evicted_pods}
+	do
+    		kubectl delete pods "$pod"
+	done
 fi
+
